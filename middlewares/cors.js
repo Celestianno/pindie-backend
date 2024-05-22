bb177d637a09a81d85e665ae90a4a58c92714748
Celestianno <crazysingularity@gmail.com>
2024-05-23 00:06:01 +0300

    change cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,8 @@ const allowedCors = [
     "https://practicum.yandex.ru",
     "https://students-projects.ru",
     "http://localhost:3000",
-    "http://localhost:3001"
+    "http://localhost:3001",
+    "https://pindie-indie-games.nomoredomainswork.ru"
 ];
 
 function cors(req, res, next) {
@@ -10,9 +11,10 @@ function cors(req, res, next) {
 
     if(allowedCors.includes(origin)) {
         res.header("Access-Control-Allow-Origin", origin);
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     }
+
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     
     next();
 }
